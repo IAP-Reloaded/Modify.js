36a2a9c8aea2681a09edbc532f24e164089f5d97
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-14 18:50:39 -0400

    Create Modify-Pull.js

---
+++ Modify-Pull.js
@@ -0,0 +1,32 @@
+var mdebug = false;
+var moduleurl = "";
+
+function enableModifyDebugging() {
+  mdebug = true;
+}
+
+function disableModifyDebugging() {
+  mdebug = false;
+}
+
+function toggleModifyDebugging() {
+  mdebug = !mdebug;
+}
+
+function testModifyDebugging() {
+  if (mdebug == true) {
+    console.log("Debugging in Modify.js is enabled.");
+  }
+}
+
+function pullModule(module) {
+  moduleurl = "https://raw.githubusercontent.com/IAP-Reloaded/Modify.js/master/" + module + ".js";
+  if (mdebug == true) {
+    console.log("Pulling module " + module + " from GitHub resp... (Full URL for it was " + moduleurl + ")");
+  }
+  var imported = document.createElement('script');
+  imported.src = moduleurl;
+  document.head.appendChild(imported);
+  if (mdebug == true) {
+    console.log("Finished pulling module " + module + "! (Full URL for it was " + moduleurl + ")");
+  }
